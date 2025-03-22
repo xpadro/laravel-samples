9b43fdcbd9cd0fad1e421e1e22d32ecd77e45e32
xpadro <xpadro.sw@gmail.com>
2025-03-22 09:26:28 +0100

    move connection props to file

---
+++ mysql-basic/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    public $connection;
+
+    public function __construct($config, $username = 'root', $password = '') {
+        $dsn = "mysql:" . http_build_query($config, '', ';');
+
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+        $this->connection = new PDO($dsn, $username, $password, $options);
+    }
+
+    public function query($query) {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        
+        return $statement;
+    }
+}
--- mysql-basic/controllers/index.php
+++ mysql-basic/controllers/index.php
@@ -1,13 +1,11 @@
 <?php
 
+require 'Database.php';
+
+$config = require 'config.php';
+
+$database = new Database($config['database']);
+$posts = $database->query("select * from posts")->fetchAll();
+
 $heading = 'Home';
-
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 require 'views/index.view.php';
